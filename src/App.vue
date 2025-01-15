9c29752a1528361cdb712099c7418ff084ce7635
Akilhan <oqiljondadaxanov@gmail.com>
2025-01-15 23:25:05 +0500

    feat: show full & less description

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
 <template>
-  <h1 class="underline uppercase text-red-500 bg-blue-500">Vue jobs</h1>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
